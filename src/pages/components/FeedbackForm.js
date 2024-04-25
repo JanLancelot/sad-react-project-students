a8e3af3ef69b00a0a7da9ce268179f982bf839e5
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-04-25 10:26:39 +0800

    Form v5

--- src/pages/components/FeedbackForm.js
+++ src/pages/components/FeedbackForm.js
@@ -258,6 +258,7 @@ const EvalForm = () => {
         <div className="flex items-center justify-between">
           <button
             type="submit"
+            onClick={handleSubmit}
             className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Submit
